b3a8bbad827a648956057ebe7a806dbe15617788
myjyby <myjyby@gmail.com>
2026-04-26 18:40:11 +0200

    added abs path

--- __scripts__/__main__.py
+++ __scripts__/__main__.py
@@ -1,12 +1,25 @@
 from json import loads
 # from sys import path as syspath, argv
-from os import makedirs, listdir
-from os.path import join, exists, dirname, splitext
+from os import makedirs, listdir, getcwd
+from os.path import join, exists, dirname, splitext, relpath
 from shutil import copyfile
 from re import sub
+from pathlib import Path
 
 def makeSafe (path):
   return sub(r'[^a-z0-9]', '_', path.lower())
+
+def reconstructPath (string):
+  path = sub(r"[\{\}]+", "", string)
+  return join("/jyby/", path[1:])
+
+def resolveAbsPaths (file):
+  html = ""
+  # print(root)
+  for line in file:
+    line = sub(r"{{[^}]*}}", lambda x: reconstructPath(x.group()), line)
+    html += line
+  return html
 
 def run ():
   """
@@ -23,7 +36,12 @@ def run ():
     page_dir = join(pages_dir, category_page.lower())
     if not exists(page_dir):
       makedirs(page_dir)
-    copyfile(join(pages_dir, "__template__.html"), join(page_dir, "index.html"))
+
+    ## Resolve the absolute paths
+    with open(join(page_dir, "index.html"), 'w') as outfile, open(join(pages_dir, "__template__.html"), 'r', encoding='utf-8') as infile:
+      html = resolveAbsPaths(infile)
+      outfile.write(html)
+    # copyfile(join(pages_dir, "__template__.html"), join(page_dir, "index.html"))
 
     data = loads(open(join(root, "public/data/", k)).read())
     for d in data:
@@ -32,7 +50,12 @@ def run ():
         content_page_dir = join(page_dir, content_page.lower())
         if not exists(content_page_dir):
           makedirs(content_page_dir)
-        copyfile(join(pages_dir, "__template__.html"), join(content_page_dir, "index.html"))
+
+        ## Resolve the absolute paths
+        with open(join(content_page_dir, "index.html"), 'w') as outfile, open(join(pages_dir, "__template__.html"), 'r', encoding='utf-8') as infile:
+          html = resolveAbsPaths(infile)
+          outfile.write(html)
+        # copyfile(join(pages_dir, "__template__.html"), join(content_page_dir, "index.html"))
 
 if __name__ == "__main__":
   run()
